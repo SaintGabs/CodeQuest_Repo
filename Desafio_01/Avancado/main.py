707e84cde1c46a092a634d6f99c23a48e802d654
Gabriel <agentecpf222222222@gmail.com>
2026-04-10 01:35:15 -0300

    site

---
+++ Desafio_01/Avancado/main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')#criar pagina
+def hello_world():#mostrar
+    return render_template('homepage.html')
+
+
+if __name__ == '__main__': #Só roda se estiver sendo executado por aq
+    app.run(debug=True)
